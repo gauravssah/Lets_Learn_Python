f070d9c5d6e370632a8f44a28aa46af196ed0cec
gauravssah <124705806+gauravssah@users.noreply.github.com>
2025-03-12 19:30:13 +0530

    running py

---
+++ 02 CHW 100Days Of Python/06 Variables and Data types/main.py
@@ -0,0 +1,6 @@
+# variables and data types 
+
+a = 520
+print("Printing a:",a) 
+
+print(type(a)) 
---
+++ 02 CHW 100Days Of Python/10 Taking User Inputs/main.py
@@ -0,0 +1,10 @@
+print("Hey Welcome Back.")
+
+a = input("Hey Enter Your Name: ")
+b = input("Enter your Year of DOB: ")
+c= input("Enter First Number: ")
+d= input("Enter Second Number: ")
+
+print("Your Name is :",a,",\nYour Year of DOB is: ",b, ",\nType of Year is :", type(b), ",\nThe sum of your two number is : ", int(c)+ int(d))
+
+
